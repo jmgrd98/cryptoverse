f6cdfd50d6cf13017ec721401b9a5b95f5487d44
João Marcelo Dantas <jmgrd98@gmail.com>
2023-08-04 14:05:14 -0300

    Update CryptoDetails.jsx

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -104,7 +104,81 @@ export default function CryptoDetails() {
                     {cryptoDetails?.name} live price in US dollars.
                     View value statistics, market cap and supply.
                 </p>
-
+            </Col>
+            <Select
+                defaultValue={'7d'}
+                className={'select-timeperiod'}
+                placeholder={'Select Time Period'}
+                onChange={(value) => setTimePeriod(value)}
+            >
+                {time.map((date) => <Option key={date}>{date}</Option>)}
+            </Select>
+            {/* Line Chart */}
+            <Col className={'stats-container'}>
+                <Col className={'coin-value-statistics'}>
+                    <Col className={'coin-value-statistics-heading'}>
+                        <Title level={3} className={'coin-details-heading'}>
+                            {cryptoDetails?.name} Value Statistics
+                        </Title>
+                        <p>
+                            An overview showing the statistics of {cryptoDetails?.name}, such as the base and quote
+                            currency, the rank, and trading volume.
+                        </p>
+                    </Col>
+                    {stats.map(({icon, title, value}) => (
+                        <Col key={''} className={'coin-stats'}>
+                            <Col className={'coin-stats-name'}>
+                                <Text>{icon}</Text>
+                                <Text>{title}</Text>
+                            </Col>
+                            <Text className={'stats'}>{value}</Text>
+                        </Col>
+                    ))}
+                </Col>
+                <Col className={'other-stats-info'}>
+                    <Col className={'coin-value-statistics-heading'}>
+                        <Title level={3} className={'coin-details-heading'}>
+                            Other Stats Info
+                        </Title>
+                        <p>
+                            An overview showing the statistics of {cryptoDetails?.name}, such as the base and quote
+                            currency, the rank, and trading volume.
+                        </p>
+                    </Col>
+                    {genericStats.map(({icon, title, value}) => (
+                        <Col className={'coin-stats'}>
+                            <Col className={'coin-stats-name'}>
+                                <Text>{icon}</Text>
+                                <Text>{title}</Text>
+                            </Col>
+                            <Text className={'stats'}>{value}</Text>
+                        </Col>
+                    ))}
+                </Col>
+            </Col>
+            <Col className={'coin-desc-link'}>
+                <Row className={'coin-desc'}>
+                    <Title level={3} className={'coin-details-heading'}>
+                        What is {cryptoDetails?.name}
+                        {HTMLReactParser(cryptoDetails?.description)}
+                    </Title>
+                </Row>
+                <Col className={'coin-links'}>
+                    <Title level={3} className={'coin-details-heading'}>
+                        {cryptoDetails?.name} Links
+                    </Title>
+                    {cryptoDetails?.links?.map((link) => (
+                        <Row className={'coin-link'} key={link.name}>
+                            <Title level={5} className={'link-name'}>
+                                {link.type}
+                            </Title>
+                            <a href={link.url} target={'_blank'} rel={'noreferrer'}>
+                                {link.name}
+                            </a>
+                        </Row>
+                    ))}
+                </Col>
+                );
             </Col>
         </Col>
     );
